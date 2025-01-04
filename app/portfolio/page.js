d05c89c9c204ab4ee73109b035d052ee6f44cbf5
femiyb <femiyb@gmail.com>
2025-01-04 23:29:43 +0200

    Add .editorconfig with normalized LF line endings

---
+++ app/portfolio/page.js
@@ -0,0 +1,14 @@
+import Header from "@/components/common/Header";
+import Footer from "@/components/common/Footer";
+
+export default function ContactPage() {
+  return (
+    <>
+      <main className="py-16 px-4 bg-gray-50">
+        <h1 className="text-4xl font-bold text-center text-teal-500 mb-8">
+          Portfolio 
+        </h1>
+      </main>
+    </>
+  );
+}
