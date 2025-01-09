cb52f6961015598497fd5652c77723509da8dd4a
femiyb <femiyb@gmail.com>
2025-01-09 23:50:57 +0200

    Styling, Change Image Path

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,6 @@
-import AboutHero from "@/components/about/AboutHero";
-import AboutIntro from "@/components/about/AboutIntro";
-import AboutContent from "@/components/about/AboutContent";
+import AboutHero from '@/components/about/AboutHero';
+import AboutIntro from '@/components/about/AboutIntro';
+import AboutContent from '@/components/about/AboutContent';
 
 export default function AboutPage() {
   return (
---
+++ app/api/image/[...path].js
@@ -0,0 +1,30 @@
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+// WordPress uploads base URL
+const IMAGE_BASE_URL = "https://www.femiyb.com/wp-content/uploads";
+
+export const config = {
+  api: {
+    bodyParser: false, // Disable body parsing for proxy middleware
+  },
+};
+
+// Export the proxy middleware
+export default createProxyMiddleware({
+  target: IMAGE_BASE_URL, // Target URL for proxying
+  changeOrigin: true, // Changes the origin of the host header to the target URL
+  pathRewrite: {
+    "^/api/image": "", // Removes "/api/image" from the request path
+  },
+  onProxyReq: (proxyReq, req, res) => {
+    console.log("Original Request Path:", req.url); // Logs the incoming request URL
+    console.log("Proxied Path:", proxyReq.path); // Logs the rewritten path
+  },
+  onProxyRes: (proxyRes, req, res) => {
+    console.log("Response received with status:", proxyRes.statusCode); // Logs the response status
+  },
+  onError: (err, req, res) => {
+    console.error("Proxy Error:", err.message); // Logs errors
+    res.status(500).send("Proxy error occurred");
+  },
+});
